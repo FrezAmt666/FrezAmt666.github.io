8b36c633238954478cdc641898638283cde11cec
FrezAmt666 <aungmyatthu20031@gmail.com>
2022-12-25 23:08:17 +0630

    mama

--- Restaurant_project/js/script.js
+++ Restaurant_project/js/script.js
@@ -35,6 +35,85 @@ $(document).ready(function () {
           // instead of a settings object
         ]
       });
+
+      $("#all").click(function() { 
+        $(".local").show("slow");
+        $(".asian").show("slow");
+        $(".european").show("slow");
+        $(".drink").show("slow");
+
+
+        $("#all").addClass("choose");
+        $("#local").removeClass("choose");
+        $("#asian").removeClass("choose");
+        $("#european").removeClass("choose");
+        $("#drink").removeClass("choose");
+      
+      });
+
+      $("#local").click(function() { 
+        $(".local").show("slow");
+        $(".asian").hide("slow");
+        $(".european").hide("slow");
+        $(".drink").hide("slow");
+
+
+        $("#all").removeClass("choose");
+        $("#local").addClass("choose");
+        $("#asian").removeClass("choose");
+        $("#european").removeClass("choose");
+        $("#drink").removeClass("choose");
+      
+      });
+
+      $("#asian").click(function() { 
+        $(".local").hide("slow");
+        $(".asian").show("slow");
+        $(".european").hide("slow");
+        $(".drink").hide("slow");
+
+
+        $("#all").removeClass("choose");
+        $("#local").removeClass("choose");
+        $("#asian").addClass("choose");
+        $("#european").removeClass("choose");
+        $("#drink").removeClass("choose");
+      
+      });
+
+      $("#european").click(function() { 
+        $(".local").hide("slow");
+        $(".asian").hide("slow");
+        $(".european").show("slow");
+        $(".drink").hide("slow");
+
+
+        $("#all").removeClass("choose");
+        $("#local").removeClass("choose");
+        $("#asian").removeClass("choose");
+        $("#european").addClass("choose");
+        $("#drink").removeClass("choose");
+      
+      });
+
+      $("#drink").click(function() { 
+        $(".local").hide("slow");
+        $(".asian").hide("slow");
+        $(".european").hide("slow");
+        $(".drink").show("slow");
+
+
+        $("#all").removeClass("choose");
+        $("#local").removeClass("choose");
+        $("#asian").removeClass("choose");
+        $("#european").removeClass("choose");
+        $("#drink").addClass("choose");
+      
+      });
+
+      
+
+      
       
     
 
