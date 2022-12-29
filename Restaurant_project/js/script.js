2eb4421029cc7ea4f6f441356d7befbba0ef2ef0
FrezAmt666 <aungmyatthu20031@gmail.com>
2022-12-29 23:36:16 +0630

    mama

--- Restaurant_project/js/script.js
+++ Restaurant_project/js/script.js
@@ -111,6 +111,524 @@ $(document).ready(function () {
       
       });
 
+      $("#bichicken").show();
+      $("#bipork").hide();
+      $("#bibimchicken").addClass("select");
+      $("#bibimpork").removeClass("select");
+      $("#bibimchicken").click(function () {
+        $("#bibimchicken").addClass("select");
+        $("#bibimpork").removeClass("select");
+        $("#bichicken").show();
+        $("#bipork").hide();
+        });
+
+      $("#bibimpork").click(function () {
+        $("#bibimpork").addClass("select");
+        $("#bibimchicken").removeClass("select");
+        $("#bichicken").hide();
+        $("#bipork").show();
+        });
+
+
+        //pizza
+        $(".smallpizza").show();
+        $(".mediumpizza").hide();
+        $(".largepizza").hide();
+        $("#small").click(function() { 
+          $(".smallpizza").show();
+          $(".mediumpizza").hide();
+          $(".largepizza").hide();
+         
+  
+  
+          $("#small").addClass("choose");
+          $("#medium").removeClass("choose");
+          $("#large").removeClass("choose");
+          
+        
+        });
+
+        $("#medium").click(function() { 
+          $(".smallpizza").hide();
+          $(".mediumpizza").show();
+          $(".largepizza").hide();
+         
+  
+  
+          $("#small").removeClass("choose");
+          $("#medium").addClass("choose");
+          $("#large").removeClass("choose");
+          
+        
+        });
+
+        $("#large").click(function() { 
+          $(".smallpizza").hide();
+          $(".mediumpizza").hide();
+          $(".largepizza").show();
+         
+  
+  
+          $("#small").removeClass("choose");
+          $("#medium").removeClass("choose");
+          $("#large").addClass("choose");
+          
+        
+        });
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+
+
+          // small pizza
+        $("#chickenpizza1").click(function (){
+          $("#psmall1").show();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza1").addClass("choose");
+          $("#porkpizza1").removeClass("choose");
+          $("#beefpizza1").removeClass("choose");
+          $("#seafoodpizza1").removeClass("choose");
+          $("#sausagepizza1").removeClass("choose");
+        });
+
+        $("#porkpizza1").click(function (){
+          $("#psmall1").hide();
+          $("#psmall2").show();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+          
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza1").removeClass("choose");
+          $("#porkpizza1").addClass("choose");
+          $("#beefpizza1").removeClass("choose");
+          $("#seafoodpizza1").removeClass("choose");
+          $("#sausagepizza1").removeClass("choose");
+        });
+
+        $("#beefpizza1").click(function (){
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").show();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza1").removeClass("choose");
+          $("#porkpizza1").removeClass("choose");
+          $("#beefpizza1").addClass("choose");
+          $("#seafoodpizza1").removeClass("choose");
+          $("#sausagepizza1").removeClass("choose");
+        });
+
+        $("#seafoodpizza1").click(function (){
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").show();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza1").removeClass("choose");
+          $("#porkpizza1").removeClass("choose");
+          $("#beefpizza1").removeClass("choose");
+          $("#seafoodpizza1").addClass("choose");
+          $("#sausagepizza1").removeClass("choose");
+        });
+
+        $("#sausagepizza1").click(function (){
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").show();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza1").removeClass("choose");
+          $("#porkpizza1").removeClass("choose");
+          $("#beefpizza1").removeClass("choose");
+          $("#seafoodpizza1").removeClass("choose");
+          $("#sausagepizza1").addClass("choose");
+        });
+        //small pizza end
+        
+        // medium pizza
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+
+        $("#chickenpizza2").click(function (){
+          $("#pmedium1").show();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza2").addClass("choose");
+          $("#porkpizza2").removeClass("choose");
+          $("#beefpizza2").removeClass("choose");
+          $("#seafoodpizza2").removeClass("choose");
+          $("#sausagepizza2").removeClass("choose");
+        });
+
+        $("#porkpizza2").click(function (){
+          $("#pmedium1").hide();
+          $("#pmedium2").show();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza2").removeClass("choose");
+          $("#porkpizza2").addClass("choose");
+          $("#beefpizza2").removeClass("choose");
+          $("#seafoodpizza2").removeClass("choose");
+          $("#sausagepizza2").removeClass("choose");
+        });
+
+        $("#beefpizza2").click(function (){
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").show();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza2").removeClass("choose");
+          $("#porkpizza2").removeClass("choose");
+          $("#beefpizza2").addClass("choose");
+          $("#seafoodpizza2").removeClass("choose");
+          $("#sausagepizza2").removeClass("choose");
+        });
+
+        $("#seafoodpizza2").click(function (){
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").show();
+          $("#pmedium5").hide();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza2").removeClass("choose");
+          $("#porkpizza2").removeClass("choose");
+          $("#beefpizza2").removeClass("choose");
+          $("#seafoodpizza2").addClass("choose");
+          $("#sausagepizza2").removeClass("choose");
+        });
+
+        $("#sausagepizza2").click(function (){
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").show();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#chickenpizza2").removeClass("choose");
+          $("#porkpizza2").removeClass("choose");
+          $("#beefpizza2").removeClass("choose");
+          $("#seafoodpizza2").removeClass("choose");
+          $("#sausagepizza2").addClass("choose");
+        });
+        // medium pizza end
+
+
+        // large pizza
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+        $("#chickenpizza3").click(function (){
+          $("#plarge1").show();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+          
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+          
+          $("#chickenpizza3").addClass("choose");
+          $("#porkpizza3").removeClass("choose");
+          $("#beefpizza3").removeClass("choose");
+          $("#seafoodpizza3").removeClass("choose");
+          $("#sausagepizza3").removeClass("choose");
+        });
+
+        $("#porkpizza3").click(function (){
+          $("#plarge1").hide();
+          $("#plarge2").show();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+          
+          $("#chickenpizza3").removeClass("choose");
+          $("#porkpizza3").addClass("choose");
+          $("#beefpizza3").removeClass("choose");
+          $("#seafoodpizza3").removeClass("choose");
+          $("#sausagepizza3").removeClass("choose");
+        });
+
+        $("#beefpizza3").click(function (){
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").show();
+          $("#plarge4").hide();
+          $("#plarge5").hide();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+          
+          $("#chickenpizza3").removeClass("choose");
+          $("#porkpizza3").removeClass("choose");
+          $("#beefpizza3").addClass("choose");
+          $("#seafoodpizza3").removeClass("choose");
+          $("#sausagepizza3").removeClass("choose");
+        });
+
+        $("#seafoodpizza3").click(function (){
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").show();
+          $("#plarge5").hide();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+          
+          $("#chickenpizza3").removeClass("choose");
+          $("#porkpizza3").removeClass("choose");
+          $("#beefpizza3").removeClass("choose");
+          $("#seafoodpizza3").addClass("choose");
+          $("#sausagepizza3").removeClass("choose");
+        });
+
+        $("#sausagepizza3").click(function (){
+          $("#plarge1").hide();
+          $("#plarge2").hide();
+          $("#plarge3").hide();
+          $("#plarge4").hide();
+          $("#plarge5").show();
+
+          $("#psmall1").hide();
+          $("#psmall2").hide();
+          $("#psmall3").hide();
+          $("#psmall4").hide();
+          $("#psmall5").hide();
+
+          $("#pmedium1").hide();
+          $("#pmedium2").hide();
+          $("#pmedium3").hide();
+          $("#pmedium4").hide();
+          $("#pmedium5").hide();
+          
+          $("#chickenpizza3").removeClass("choose");
+          $("#porkpizza3").removeClass("choose");
+          $("#beefpizza3").removeClass("choose");
+          $("#seafoodpizza3").removeClass("choose");
+          $("#sausagepizza3").addClass("choose");
+        });
+
+        // large pizza end
+
 
 
       //detail price
@@ -259,12 +777,17 @@ $(document).ready(function () {
 
     $('#order').click(function(e){
       e.preventDefault();
+      $("#alert").text("You have ordered successfully!");
+      $("#alert").addClass("alert-success");
       localStorage.clear();
       getData();
       count();
     });
 
   });
+
+
+  
       
       
     
